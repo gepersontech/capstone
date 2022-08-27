83ca2543621d53d5869db3a197af4f750458809f
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-08-27 15:04:01 +0800

    upadate

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 
         <div class="row">
 
-          <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+          <!-- <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
             <div class="info">
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
@@ -37,31 +37,32 @@
 
             </div>
 
-          </div>
+          </div> -->
 
-          <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="200">
+          <div class="col-lg-4 mt-5 mt-lg-0 " style=" margin:auto;" data-aos="fade-left" data-aos-delay="200">
 
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
-                <div class="col-md-6 form-group">
+                <div class="form-group mt-3">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
+                <!-- <div class="col-md-6 form-group mt-3 mt-md-0">
                   <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
+                </div> -->
               </div>
               <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                <input type="password" class="form-control" name="subject" id="subject" placeholder="Password" required>
               </div>
-              <div class="form-group mt-3">
+              <!-- <div class="form-group mt-3">
                 <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
+              </div> -->
+              <!-- <div class="my-3">
                 <div class="loading">Loading</div>
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              </div> --> <br> <br>
+              
+              <div class="text-center"><button type="submit" class="btn btn-primary">Login</button></div>
             </form>
 
           </div>
