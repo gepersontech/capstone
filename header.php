7b386ff1167469177d3fb710c08b5e26416e7a13
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-09-01 00:23:57 +0800

    update

--- header.php
+++ header.php
@@ -55,27 +55,6 @@
           <li><a class="nav-link scrollto" href="#about">About Us</a></li>
           <li><a class="nav-link scrollto" href="#about">Lesson</a></li>
           <li><a class="nav-link scrollto" href="video/video.html">Lesson</a></li>
-
-          <!-- <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> -->
-          
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
 
           <!-- <li><a href=""><i class="bi bi-search "></i></a></li> -->
