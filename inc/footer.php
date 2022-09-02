7754930bb6b5dc205b769351ddfdf2528ff4477e
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-09-02 23:24:10 +0800

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -83,7 +83,7 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/ -->
-        Designed by <a href="#">Intelligent Tutor</a>
+        Designed by <a href="index.php">Intelligent Tutor</a>
       </div>
     </div>
   </footer>
