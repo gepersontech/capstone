3671bfc5d59c8e0733f07edf55a8ba70474d9804
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-10-29 21:33:33 +0800

    back-end

---
+++ conf/config.php
@@ -0,0 +1,30 @@
+<?php
+    // setting time zone
+    // if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    //     header('Location: ../index.php?session=expired');
+    // }
+    date_default_timezone_set("Asia/Manila");
+
+    // define the site root
+    define('SITE_ROOT', 'http://localhost/property/');
+
+    /* Database credentials. Assuming you are running MySQL
+    server with default setting (user 'root' with no password) */
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'pimos_db');
+    
+    /* Attempt to connect to MySQL database */
+    $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Check connection
+    if($con === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+    //alert('$message');
+//     function displayAlert($title,$text,$type,$icon){
+    
+// }
+    
+?>
