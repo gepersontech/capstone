85215d780991f518363449c6862758fcaa8e965f
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-11-04 21:31:54 +0800

    Update course.php

--- app/course.php
+++ app/course.php
@@ -30,21 +30,6 @@
                 </tr>
             </thead>
             <tbody>
-                <!-- <tr>
-                    <td>GEOMETRY</td>
-                    <td>
-                        <div class="dropdown">
-                            <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#"
-                                role="button" data-toggle="dropdown">
-                                <i class="dw dw-more"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                                <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> Update</a>
-                                <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
-                            </div>
-                        </div>
-                    </td>
-                </tr> -->
 
                 <?php
                 $result = mysqli_query($con,"SELECT * FROM `course`");
@@ -72,10 +57,11 @@
                 }
                     
                 }else{
-                    echo "No Records Found";
+                    //echo "No Records Found";
                 }
-                
+                 echo "No Records Found";
             ?>
+
             </tbody>
         </table>
     </div>
