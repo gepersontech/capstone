cc433df76dbbeb60a0764a9820915526c89bdf0c
Geraint Lloyd Ortiz <108078862+Kerygma1279@users.noreply.github.com>
2022-12-04 21:37:26 +0800

    update

--- app/faq.php
+++ app/faq.php
@@ -37,130 +37,132 @@
             </div>
         </div> -->
         <div class="faq-wrap">
-            <h4 class="mb-20 h4 text-blue">Accordion example</h4>
+            <h4 class="mb-20 h4 text-blue">Lesson 1</h4>
             <div id="accordion">
                 <div class="card">
                     <div class="card-header">
                         <button class="btn btn-block" data-toggle="collapse" data-target="#faq1">
-                            Collapsible Group Item #1
+                        What I need to Know
                         </button>
                     </div>
                     <div id="faq1" class="collapse show" data-parent="#accordion">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life
-                            accusamus terry richardson ad squid. 3 wolf moon officia
-                            aute, non cupidatat skateboard dolor brunch. Food truck
-                            quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                            sunt aliqua put a bird on it squid single-origin coffee
-                            nulla assumenda shoreditch et. Nihil anim keffiyeh
-                            helvetica, craft beer labore wes anderson cred nesciunt
-                            sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                            Leggings occaecat craft beer farm-to-table, raw denim
-                            aesthetic synth nesciunt you probably haven't heard of them
-                            accusamus labore sustainable VHS.
+                        > illustrate the different types of conic sections: parabola, ellipse, circle,
+                        hyperbola, and degenerate cases;<br>
+                        > determine the type of conic section defined by a given 2nd degree
+                        equation in x and y;<br>
+                        > define a circle;<br>
+                        > determine the standard form of equation of circle;<br>
+                        > Graph a circle in a rectangular coordinate system;<br>
+                        > Derive and illustrate the equation of the circle;<br>
+                        > Find the center and the radius of the circle of an equation;<br>
+                        > Convert the general equation of circle to standard form and vice versa;<br>
                         </div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header">
                         <button class="btn btn-block collapsed" data-toggle="collapse" data-target="#faq2">
-                            Collapsible Group Item #2
+                        What’s In
                         </button>
                     </div>
                     <div id="faq2" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life
-                            accusamus terry richardson ad squid. 3 wolf moon officia
-                            aute, non cupidatat skateboard dolor brunch. Food truck
-                            quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                            sunt aliqua put a bird on it squid single-origin coffee
-                            nulla assumenda shoreditch et. Nihil anim keffiyeh
-                            helvetica, craft beer labore wes anderson cred nesciunt
-                            sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                            Leggings occaecat craft beer farm-to-table, raw denim
-                            aesthetic synth nesciunt you probably haven't heard of them
-                            accusamus labore sustainable VHS.
+                        <p>Activity 1: Recall
+                        You had learned in your previous mathematics in junior high school about
+                        solving a quadratic equation by completing the squares. Let us recall your
+                        knowledge about the subject using these following examples.
+                        In order to find the roots of a certain quadratic equation, the following steps
+                        will be used using completing the square method.</p>
+
+                        <img src="images/lesson1.png" width="700" height="500" />
+
                         </div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header">
                         <button class="btn btn-block collapsed" data-toggle="collapse" data-target="#faq3">
-                            Collapsible Group Item #3
+                        What’s New
+
                         </button>
                     </div>
                     <div id="faq3" class="collapse" data-parent="#accordion">
                         <div class="card-body">
                             <p>
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                accusamus terry richardson ad squid. 3 wolf moon officia
-                                aute, non cupidatat skateboard dolor brunch. Food truck
-                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                                tempor, sunt aliqua put a bird on it squid single-origin
-                                coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                                helvetica, craft beer labore wes anderson cred nesciunt
-                                sapiente ea proident. Ad vegan excepteur butcher vice
-                                lomo. Leggings occaecat craft beer farm-to-table, raw
-                                denim aesthetic synth nesciunt you probably haven't heard
-                                of them accusamus labore sustainable VHS.
+                            Geometric Figures or shapes are use in architectural designs. For this activity,
+                            identify the following shapes as circle, parabola, ellipse, or hyperbola as shown in
+                            the pictures being used in real-life. Write your answer on the space provided.
                             </p>
-                            <p class="mb-0">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                accusamus terry richardson ad squid. 3 wolf moon officia
-                                aute, non cupidatat skateboard dolor brunch. Food truck
-                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                                tempor, sunt aliqua put a bird on it squid single-origin
-                                coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                                helvetica, craft beer labore wes anderson cred nesciunt
-                                sapiente ea proident. Ad vegan excepteur butcher vice
-                                lomo. Leggings occaecat craft beer farm-to-table, raw
-                                denim aesthetic synth nesciunt you probably haven't heard
-                                of them accusamus labore sustainable VHS.
+                           <center><img src="images/lesson2.png" width="700" height="500" /></center>
+                            
+                            <p>
+                            Does the activity ignite your interest to study more about geometric shapes
+                            particularly different conic sections like your answers in the activity? Can you name
+                            other architectural designs not in the pictures that used the idea of geometrical
+                            shapes? Does the shape matters on the durability, functionality and artistic designs?
+                            Studying this module will help you appreciate nature and man’s creation that
+                            would help daily life activities.<br>
+
                             </p>
+
+
                         </div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header">
                         <button class="btn btn-block collapsed" data-toggle="collapse" data-target="#faq4">
-                            Collapsible Group Item #4
+                        What is It
                         </button>
                     </div>
                     <div id="faq4" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life
-                            accusamus terry richardson ad squid. 3 wolf moon officia
-                            aute, non cupidatat skateboard dolor brunch. Food truck
-                            quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                            sunt aliqua put a bird on it squid single-origin coffee
-                            nulla assumenda shoreditch et. Nihil anim keffiyeh
-                            helvetica, craft beer labore wes anderson cred nesciunt
-                            sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                            Leggings occaecat craft beer farm-to-table, raw denim
-                            aesthetic synth nesciunt you probably haven't heard of them
-                            accusamus labore sustainable VHS.
+                           <p style = "text-indent:50px;"> We present the conic sections, a particular class of curves which sometimes
+                        appear in nature and which have applications in other fields. In this lesson, we first
+                        illustrate how each of these curves is obtained from the intersection of a plane and a
+                        cone, and then discuss the first of their kind, circles. The other conic sections will be
+                        covered in the next lessons.<br>
+                        <p style = "text-indent:50px;"><b>Conic sections</b> (or conics), is a curved formed by a plane passing through a
+                        double-napped circular cone. One of the first shapes we learned, a circle, is a conic.
+                        When you throw a ball, the trajectory it takes is a parabola. The orbit taken by each
+                        planet around the sun is an ellipse. Properties of hyperbolas have been used in the
+                        design of certain telescopes and navigation systems. We will discuss circles in this
+                        lesson, leaving parabolas, ellipses, and hyperbolas for subsequent lessons.<br></p>
+                        <p>
+                      
+                        <ul style = "text-indent:50px;">
+                            <li><b>Circle</b> (Figure 1.1) – is a special case of ellipse in which the plane is perpendicular to the axis of the cone.</li><br>
+                            <li><b>Ellipse</b> (Figure 1.1) - when the (tilted) plane intersects only one cone to form a bounded curve</li><br>
+                            <li><b>Parabola</b> (Figure 1.2) – the plane is parallel to a generator line of the cone</li><br>
+                            <li><b>Hyperbola</b> (Figure 1.3) – the intersection is an unbounded curve and the plane is not parallel to a generator line of the cone and the plane intersects both halves of the cone.</li><br>
+                        </ul>
+                        <center><img src="images/lesson3.png" width="700" height="500" /></center>
+
+                        <p>We can draw these conic sections on a rectangular coordinate plane and find
+                        their equations. To be able to do this, we will present equivalent definitions of these
+                        conic sections in subsequent sections, and use these to find the equations.
+                        There are other ways for a plane and the cones to intersect, to form what are
+                        referred to as degenerate conics: a point, one line, and two lines. See Figures 1.4,
+                        1.5, and 1.6.
+                        </p>
+                        <center><img src="images/lesson4.png" width="700" height="500"/></center>
                         </div>
+                        
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header">
                         <button class="btn btn-block collapsed" data-toggle="collapse" data-target="#faq5">
-                            Collapsible Group Item #5
+                            Table 1
                         </button>
                     </div>
                     <div id="faq5" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life
-                            accusamus terry richardson ad squid. 3 wolf moon officia
-                            aute, non cupidatat skateboard dolor brunch. Food truck
-                            quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                            sunt aliqua put a bird on it squid single-origin coffee
-                            nulla assumenda shoreditch et. Nihil anim keffiyeh
-                            helvetica, craft beer labore wes anderson cred nesciunt
-                            sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                            Leggings occaecat craft beer farm-to-table, raw denim
-                            aesthetic synth nesciunt you probably haven't heard of them
-                            accusamus labore sustainable VHS.
+                            <center>Graphs of Quadratic Equations</center>
+                            <center><img src="images/lesson5.png" width="700" height="500" /></center>
+                            <center><p>Example1.1 Determine the type conic section that each general equation will produce.</p></center>
+                            <center><img src="images/lesson6.png" width="700" height="500" /></center>
                         </div>
                     </div>
                 </div>
