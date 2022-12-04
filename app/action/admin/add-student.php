9049f02886ddde8b9d538d99d42453a85e873bb3
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-04 21:36:41 +0800

    update

--- app/action/admin/add-student.php
+++ app/action/admin/add-student.php
@@ -2,28 +2,14 @@
     include('../../../conf/config.php');
     
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $role = $email = "";
-        // role
-        if($_POST["options"] == 0){
-            $error_msg = "Please choose usertype.";
-        }else{
-            $role = trim($_POST["options"]);
-            if($_POST["email"] != null){
-            $error_msg = "Please choose usertype.";
-
-            }else{
-                $role = trim($_POST["options"]);
-
-            }
-        }
-
-
-        //$role = $_POST['options'];
+              
+        
+        $role = isset($_POST['options']);
         $email = trim($_POST['email']);
         $username = $_POST['username'];
         $password = $_POST['password'];
         $confirmpassword = $_POST['confirmpassword'];
-        $password = password_hash($confirmpassword, PASSWORD_DEFAULT);
+        
 
         $firstname = ucwords(trim($_POST['firstname']));
         $lastname = ucwords(trim($_POST['lastname']));
@@ -33,10 +19,47 @@
         
         $date = date('Y-m-d H:i:s');
         
-        $query=mysqli_query($con,"INSERT INTO `users`(`firstname`, `lastname`, `birthdate`, `age`,`gender`,`address`,`email_add`, `username`, `password`, `role_id`, `added_at`) 
-                                            VALUES ('".$firstname."','".$lastname."','".$birthdate."',''".$age.",'".$gender."','".$address."','".$email."','".$username."','".$password."','".$role."','".$date."')" );
-    
-    //header("location: ../../../login");
+        if($role != null){
+            header("location: ../../../register.php?error=Click the button student/teacher.");
+        }else{
+            if($role == 1){
+                $role = 3;
+            }
+        }
+        if(isset($email)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }
+        if(isset($username)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }
+        if(isset($password) || isset($confirmpassword)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }else{
+            if($password != $confirmpassword){
+                header("location: ../../../register.php?error=Password not match");
+            }
+        }
+        if(isset($firstname)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }
+        if(isset($lastname)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }
+        if(isset($gender)){
+            header("location: ../../../register.php?error=Please select you gender.");
+        }
+        if(isset($birthdate)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }
+        if(isset($address)){
+            header("location: ../../../register.php?error=Invalid input.");
+        }else{
+            $password = password_hash($confirmpassword, PASSWORD_DEFAULT);
+            $query=mysqli_query($con,"INSERT INTO `users`(`firstname`, `lastname`, `birthdate`, `age`,`gender`,`address`,`email_add`, `username`, `password`, `role_id`, `added_at`) 
+                                  VALUES ('".$firstname."','".$lastname."','".$birthdate."',''".$age.",'".$gender."','".$address."','".$email."','".$username."','".$password."','".$role."','".$date."')" );
+            header("location: ../../../login");    
+        }
+        header("location: ../../../register");
     }
     
 ?>
--- register.php
+++ register.php
@@ -1,167 +1,167 @@
 <?php
 
 // Include config file
-require_once "conf/config.php";
-include('app/action/admin/add-student.php');
+// require_once "conf/config.php";
+// include('app/action/admin/add-student.php');
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // role
-    if(empty(trim($_POST["options"]))){
-        $role_err = "Please choose usertype.";
-    }else{
-        $role = trim($_POST["options"]);
-    }
+//     // role
+//     if(empty(trim($_POST["options"]))){
+//         $role_err = "Please choose usertype.";
+//     }else{
+//         $role = trim($_POST["options"]);
+//     }
 
-    // Email
-    if(empty(trim($_POST["email"]))){
-    $email_err = "Please enter your Email Address.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["email"]))){
-    $email_err = "Email can only contain letters, numbers, and underscores.";
-    } else{
-        $email = trim($_POST["email"]);
-    }
+//     // Email
+//     if(empty(trim($_POST["email"]))){
+//     $email_err = "Please enter your Email Address.";
+//     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["email"]))){
+//     $email_err = "Email can only contain letters, numbers, and underscores.";
+//     } else{
+//         $email = trim($_POST["email"]);
+//     }
 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-    $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-    $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-    // Prepare a select statement
-    $sql = "SELECT user_id FROM users WHERE username = ?";
+//     // Validate username
+//     if(empty(trim($_POST["username"]))){
+//     $username_err = "Please enter a username.";
+//     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+//     $username_err = "Username can only contain letters, numbers, and underscores.";
+//     } else{
+//     // Prepare a select statement
+//     $sql = "SELECT user_id FROM users WHERE username = ?";
     
-    if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+//     if($stmt = mysqli_prepare($con, $sql)){
+//             // Bind variables to the prepared statement as parameters
+//             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
-            $param_username = trim($_POST["username"]);
+//             // Set parameters
+//             $param_username = trim($_POST["username"]);
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
+//             // Attempt to execute the prepared statement
+//             if(mysqli_stmt_execute($stmt)){
+//                 /* store result */
+//                 mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
+//                 if(mysqli_stmt_num_rows($stmt) == 1){
+//                     $username_err = "This username is already taken.";
+//                 } else{
+//                     $username = trim($_POST["username"]);
 
-                    // Default Email Address
-                    $email = trim($username);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+//                     // Default Email Address
+//                     $email = trim($username);
+//                 }
+//             } else{
+//                 echo "Oops! Something went wrong. Please try again later.";
+//             }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
+//             // Close statement
+//             mysqli_stmt_close($stmt);
+//         }
+//     }
 
-    // Validate password
-        if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-        } elseif(strlen(trim($_POST["password"])) < 6){
-            $password_err = "Password must have atleast 6 characters.";
-        } else{
-            $password = trim($_POST["password"]);
-        }
+//     // Validate password
+//         if(empty(trim($_POST["password"]))){
+//         $password_err = "Please enter a password.";     
+//         } elseif(strlen(trim($_POST["password"])) < 6){
+//             $password_err = "Password must have atleast 6 characters.";
+//         } else{
+//             $password = trim($_POST["password"]);
+//         }
     
-    // Validate confirm password
-    if(empty(trim($_POST["confirmpassword"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirmpassword"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
+//     // Validate confirm password
+//     if(empty(trim($_POST["confirmpassword"]))){
+//         $confirm_password_err = "Please confirm password.";     
+//     } else{
+//         $confirm_password = trim($_POST["confirmpassword"]);
+//         if(empty($password_err) && ($password != $confirm_password)){
+//             $confirm_password_err = "Password did not match.";
+//         }
+//     }
 
-    // Firstname
-    if(empty(trim($_POST["firstname"]))){
-    $firstname_err = "Please enter a firstname.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["firstname"]))){
-    $ufirstname_err = "Firstname can only contain letters, numbers, and underscores.";
-    } else{
-        $firstname = ucwords(trim($_POST["firstname"]));
-    }
+//     // Firstname
+//     if(empty(trim($_POST["firstname"]))){
+//     $firstname_err = "Please enter a firstname.";
+//     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["firstname"]))){
+//     $ufirstname_err = "Firstname can only contain letters, numbers, and underscores.";
+//     } else{
+//         $firstname = ucwords(trim($_POST["firstname"]));
+//     }
 
-        // Lastname
-        if(empty(trim($_POST["lastname"]))){
-        $lastname_err = "Please enter a lastname.";
-        } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["lastname"]))){
-        $ulastname_err = "Lastname can only contain letters, numbers, and underscores.";
-        } else{
-            $lastname = ucwords(trim($_POST["lastname"]));
-        }
+//         // Lastname
+//         if(empty(trim($_POST["lastname"]))){
+//         $lastname_err = "Please enter a lastname.";
+//         } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["lastname"]))){
+//         $ulastname_err = "Lastname can only contain letters, numbers, and underscores.";
+//         } else{
+//             $lastname = ucwords(trim($_POST["lastname"]));
+//         }
 
-        // Gender
-        if($_POST["gender"] == null){
-        $error = "Please choose your gender";
-        } else{
-            $gender = ucwords(trim($_POST["gender"]));
-        }
-        // Birtdate
-        if(empty(trim($_POST["birthdate"]))){
-        $bdate_err = "Please choose your birthdate";
-        } else{
-            $bdate = ucwords(trim($_POST["birthdate"]));
-        }
+//         // Gender
+//         if($_POST["gender"] == null){
+//         $error = "Please choose your gender";
+//         } else{
+//             $gender = ucwords(trim($_POST["gender"]));
+//         }
+//         // Birtdate
+//         if(empty(trim($_POST["birthdate"]))){
+//         $bdate_err = "Please choose your birthdate";
+//         } else{
+//             $bdate = ucwords(trim($_POST["birthdate"]));
+//         }
         
-        // Address
-        if(empty(trim($_POST["address"]))){
-        $address_err = "Enter your address";
-        } else{
-            $address = ucwords(trim($_POST["address"]));
-        }
+//         // Address
+//         if(empty(trim($_POST["address"]))){
+//         $address_err = "Enter your address";
+//         } else{
+//             $address = ucwords(trim($_POST["address"]));
+//         }
   
-  // Check input errors before inserting in database
-  if(
-    empty($email_err) && 
-    empty($role_err) && 
-    empty($username_err) && 
-    empty($password_err) && 
-    empty($confirm_password_err) && 
-    empty($firstname_err) && 
-    empty($lastname_err) && 
-    empty($birthdate_err) && 
-    empty($address_err) && 
-    empty($gender_err) ){
+//   // Check input errors before inserting in database
+//   if(
+//     empty($email_err) && 
+//     empty($role_err) && 
+//     empty($username_err) && 
+//     empty($password_err) && 
+//     empty($confirm_password_err) && 
+//     empty($firstname_err) && 
+//     empty($lastname_err) && 
+//     empty($birthdate_err) && 
+//     empty($address_err) && 
+//     empty($gender_err) ){
       
-      // Prepare an insert statement
-      $sql = "INSERT INTO users (`firstname`, `lastname`, `birthdate`, `age`,`gender`,`address`,`email_add`, 
-            `username`, `password`, `role_id`, `added_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//       // Prepare an insert statement
+//       $sql = "INSERT INTO users (`firstname`, `lastname`, `birthdate`, `age`,`gender`,`address`,`email_add`, 
+//             `username`, `password`, `role_id`, `added_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
        
 
-      $stmt=mysqli_prepare($link, $sql);
-      if(false===$stmt){
-      die('Error with prepare: ') . htmlspecialchars($mysqli->error);
-      }
+//       $stmt=mysqli_prepare($link, $sql);
+//       if(false===$stmt){
+//       die('Error with prepare: ') . htmlspecialchars($mysqli->error);
+//       }
 
-      $bp =  mysqli_stmt_bind_param($stmt, "sssssssssss",$email, $param_username, $param_password, $firstname, $lastname, $gender, $bdate, $address,  $role, $created_at, );
-          // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-      if(false===$bp){
-              die('Error with bind_param: ') . htmlspecialchars($stmt->error);
-          }
+//       $bp =  mysqli_stmt_bind_param($stmt, "sssssssssss",$email, $param_username, $param_password, $firstname, $lastname, $gender, $bdate, $address,  $role, $created_at, );
+//           // Set parameters
+//             $param_username = $username;
+//             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+//       if(false===$bp){
+//               die('Error with bind_param: ') . htmlspecialchars($stmt->error);
+//           }
 
-      $bp = $stmt->execute();
-          if ( false===$bp ) {
-              die('Error with execute: ' . htmlspecialchars($stmt->error));
-          }else{
-            header("location: login");
-          }
+//       $bp = $stmt->execute();
+//           if ( false===$bp ) {
+//               die('Error with execute: ' . htmlspecialchars($stmt->error));
+//           }else{
+//             header("location: login");
+//           }
 
-      $stmt->close();
+//       $stmt->close();
 
-  }
+//   }
   
-  // Close connection
-  mysqli_close($con);
-}
+//   // Close connection
+//   mysqli_close($con);
+// }
 
 ?>
 
@@ -220,10 +220,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
                                 <?php
-                                        
-                                        if($error_msg != null){
+                                        if(isset($_GET['error']) != null){
                                             echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                        <strong>Error!</strong> .'.$error.'.
+                                        <strong>Error!</strong> '.$_GET['error'].'
                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
@@ -312,9 +311,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                                     <label class="custom-control-label" for="male">Male</label>
                                                 </div>
                                                 <div class="custom-control custom-radio custom-control-inline pb-0">
-                                                    <input type="radio" id="female" name="gender" value="female"
+                                                    <input type="radio" id="Female" name="gender" value="female"
                                                         class="custom-control-input" />
-                                                    <label class="custom-control-label" for="female">Female</label>
+                                                    <label class="custom-control-label" for="Female">Female</label>
                                                 </div>
                                             </div>
                                         </div>
