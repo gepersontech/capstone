00bc56031b6be89498a855b50da43127e96593ab
parengjames <108077808+parengjames@users.noreply.github.com>
2022-12-05 15:51:26 +0800

    zxcvbnm,

--- app/course-user.php
+++ app/course-user.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-6 col-sm-12">
             <div class="title">
-                <h4>Manage Courses</h4>
+                <h4>Your Courses</h4>
             </div>
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb">
