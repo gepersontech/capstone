afb681b23753a93d63a566a80bb4917315c8bed5
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-07 01:42:11 +0800

    Updated

--- app/config/answerverifier.php
+++ app/config/answerverifier.php
@@ -4,18 +4,30 @@
 require_once "../../conf/config.php";
 
     if(isset($_POST['submit'])){
-        $inputanswer = $_POST['answer'];
-        $anskey = $_POST['answerkey'];
-
+            $student_id;
+            $course_id;
+            $exam_id;
+            $q = 1;
+            $inputanswer    = $_POST['answer'];
+            $anskey         = $_POST['answerkey'];
+            
         if($inputanswer == $anskey){
             $_SESSION['headertext'] = "Good job!👏";
-            $_SESSION['bodytext'] = "You got the correct answer.";
+            $_SESSION['bodytext']   = "You got the correct answer.";
             $_SESSION['statusIcon'] = "success";
-            header("location: ../examcontent");
+
+            $sql = "INSERT INTO `exam_answers`(`student_id`, `course_id`, `exam_id`, `question_id`, `num_wrong`) 
+            VALUES (1,1,1,1,1)";
+
+            $result = mysqli_query($con, $sql);
+            $q++;
+            header("location: ../index.php?page=examcontent&q=$q");
         }else{
             $_SESSION['headertext'] = "Wrong Answer!😔";
-            $_SESSION['bodytext'] = "Analyse the question more, You got this.";
+            $_SESSION['bodytext']   = "Analyse the question more, You got this.";
             $_SESSION['statusIcon'] = "error";
+
+
             header("location: ../examcontent");
         }
     }
