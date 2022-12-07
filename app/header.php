70e16c99598d779aae85816f2e780f861bbda2fd
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-07 23:40:10 +0800

    update

--- app/header.php
+++ app/header.php
@@ -21,9 +21,27 @@
     <link rel="stylesheet" type="text/css" href="vendors/styles/core.css" />
     <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css" />
     <link rel="stylesheet" type="text/css" href="src/plugins/jvectormap/jquery-jvectormap-2.0.3.css" />
-    <link rel="stylesheet" type="text/css" href="vendors/styles/style.css" /> 
+    <link rel="stylesheet" type="text/css" href="vendors/styles/style.css" />
     <!-- SweetAlert2 -->
     <!-- <link rel="stylesheet" type="text/css" href="src/plugins/sweetalert2/sweetalert2.css"/> -->
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MFXZN8Z');
+    </script>
+    <!-- End Google Tag Manager -->
 
 </head>
 
