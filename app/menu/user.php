7c0d8f90ecfed7b8f305fea97a81ef454308b432
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-10 19:18:39 +0800

    Update user.php

--- app/menu/user.php
+++ app/menu/user.php
@@ -26,45 +26,16 @@
                 <span class="micon icon-copy ti-view-list-alt"></span><span class="mtext">Take Exam</span>
             </a>
         </li>
-        <li>
+        <!-- <li>
             <a href="taken-exam" class="dropdown-toggle no-arrow">
                 <span class="micon icon-copy ti-user"></span><span class="mtext">Taken Exam</span>
             </a>
-        </li>
+        </li> -->
         <li>
             <a href="student-result" class="dropdown-toggle no-arrow">
                 <span class="micon icon-copy ti-cup"></span><span class="mtext">Student Result</span>
             </a>
         </li>
-        <!-- <li class="dropdown">
-            <a href="javascript:;" class="dropdown-toggle">
-                <span class="micon bi bi-house"></span><span class="mtext">Home</span>
-            </a>
-            <ul class="submenu">
-                <li><a href="page/index.html">Dashboard style 1</a></li>
-                <li><a href="page/index2.html">Dashboard style 2</a></li>
-                <li><a href="page/index3.html">Dashboard style 3</a></li>
-            </ul>
-        </li> -->
 
-        <!-- <li>
-            <div class="dropdown-divider"></div>
-        </li>
-        <li>
-            <div class="sidebar-small-cap">Extra</div>
-        </li>
-        <li>
-            <a href="javascript:;" class="dropdown-toggle">
-                <span class="micon bi bi-file-pdf"></span><span class="mtext">Documentation</span>
-            </a>
-            <ul class="submenu">
-                <li><a href="#">Introduction</a></li>
-                <li><a href="#">Getting Started</a></li>
-                <li><a href="#">Color Settings</a></li>
-                <li>
-                    <a href="#">Third Party Plugins</a>
-                </li>
-            </ul>
-        </li> -->
     </ul>
 </div>
