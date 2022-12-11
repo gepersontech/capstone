feb864eaad27e628c920cbdae87d8b2a9c2ba624
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-11 21:44:02 +0800

    Update header.php

--- app/header.php
+++ app/header.php
@@ -44,8 +44,6 @@
 </head>
 
 <body>
-
-
     <!-- Modal -->
     <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
