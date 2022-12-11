d2cb440563c78bd3b45fb0f04e324d0a00ed650e
Geperson Mamalias <65372946+gepersontech@users.noreply.github.com>
2022-12-11 12:57:28 +0800

    update alerts

--- login.php
+++ login.php
@@ -64,10 +64,10 @@
                         <?php
                             if(isset($_GET['error']) != null){
                                 echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                <strong>'.$_GET['error'].'</strong> 
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                                        '.$_GET['error'].'
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                 </div>';
                             }
                             
--- register.php
+++ register.php
@@ -55,7 +55,7 @@
                                 <?php
                                         if(isset($_GET['error']) != null){
                                             echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                        <strong>'.$_GET['error'].'</strong> 
+                                        '.$_GET['error'].'
                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
