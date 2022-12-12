ba761802a914c5af0ea884ebe3d8128b6bdfc58f
parengjames <108077808+parengjames@users.noreply.github.com>
2022-12-12 23:58:41 +0800

    partial results

--- app/excircle_result.php
+++ app/excircle_result.php
@@ -2,13 +2,72 @@
     <div class="row">
         <div class="col-md-6">
             <div class="title">
-                <h3>Circle Post-test Result</h3>
-                <span>Congratulation</span>
+                <h3>📜 Your Quiz Result</h3>
+                <span>Here are the overall result of your quiz.</span>
             </div>
-            <nav aria-label="breadcrumb" role="navigation">
-                <ol class="breadcrumb">
-                </ol>
-            </nav>
         </div>
     </div>
 </div>
+
+<!-- basic table  Start -->
+<div class="pd-20 card-box mb-30">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th style="font-weight: bold;font-size: 15px;">Number of attempt: </th>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <th style="font-weight: bold;font-size: 15px;">Total Question</th>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <th style="font-weight: bold;font-size: 15px;">Number of correct: </th>
+                        <td>Jacob</td>
+                    </tr>
+                    <tr style="color: #8B0000;">
+                        <th style="font-weight: bold;font-size: 15px;">Number of incorrect: </th>
+                        <td>Larry</td>
+                    </tr>
+                    <tr style="color: #FF8C00;">
+                        <th style="font-weight: bold;font-size: 15px;">Total Hint used: </th>
+                        <td>Jacob</td>
+                    </tr>
+                    <tr style="color: #32CD32;">
+                        <th style="font-weight: bold;font-size: 15px;">Passing Score: </th>
+                        <td>Larry</td>
+                    </tr>
+                    <tr>
+                        <th style="font-weight: bold;font-size: 20px;">Your Score: </th>
+                        <td style="font-size: 20px;">Larry</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <div class="content" style="height: 370px;">
+                <div class="row" style="justify-content: center;">
+                    <img style="width: 150px;margin-top: 30px;" src="../app/images/check-gif.gif" alt="clipart">
+                </div>
+                <div class="row" style="justify-content: center;">
+                    <h2>Congratulations</h2>
+                </div>
+                <div class="row" style="justify-content: center;margin-top: 10px;">
+                    <h5>You have passed the quiz</h5>
+                </div>
+
+                <!-- buttons -->
+                <div class="row" style="justify-content: center;margin-top: 30px;">
+                    <div class="col ">
+                        <button class="btn btn-success float-right">Review</button>
+                    </div>
+                    <div class="col float-right">
+                        <a href="home" class="btn btn-success">Dashboard</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- basic table  End -->
