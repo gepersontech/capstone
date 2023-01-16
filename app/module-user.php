b71ab20b02f59bea96c501ac17d952203c48b917
parengjames <108077808+parengjames@users.noreply.github.com>
2023-01-16 12:31:25 +0800

    adsad

--- app/module-user.php
+++ app/module-user.php
@@ -1,3 +1,7 @@
+    <!-- SWEET ALERT SCRIPT -->
+    <script src="vendors/sweetalert/sweetalert.min.js"></script>
+    <!-- <script src="src/plugins/sweetalert2/sweetalert2.all.js"></script> -->
+    <script src="src/plugins/sweetalert2/jquery-3.6.1.min.js"></script>
 <div class="page-header">
     <div class="row">
         <div class="col-md-6 col-sm-12">
@@ -37,31 +41,31 @@
             <tbody>
 
                 <?php
-                $result = mysqli_query($con,"SELECT * FROM `module`");
-                $count=1;
+                $result = mysqli_query($con, "SELECT * FROM `module`");
+                $count = 1;
                 $rowCount = mysqli_num_rows($result);
-                if($rowCount > 0){
-                    while($row = mysqli_fetch_assoc($result)){
-                            $id=$row['module_id'];
-                                            ?>
-                <tr>
-                    <td style="width: 300px;"><?php echo $row['module_name']; ?></td>
-                    <td style="width: 300px;"><?php echo $row['module_desc']; ?></td>
-                    <!-- <td style="width: 300px;">
+                if ($rowCount > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['module_id'];
+                ?>
+                        <tr>
+                            <td style="width: 300px;"><?php echo $row['module_name']; ?></td>
+                            <td style="width: 300px;"><?php echo $row['module_desc']; ?></td>
+                            <!-- <td style="width: 300px;">
                         
-                        <a href="index.php?page=module-edit&& id=<?php //echo $id; ?>" class=" btn btn-sm btn-primary">
+                        <a href="index.php?page=module-edit&& id=<?php //echo $id; 
+                                                                    ?>" class=" btn btn-sm btn-primary">
                             <span class="icon-copy ti-pencil-alt"></span> View
                         </a>
                     </td> -->
 
-                </tr>
+                        </tr>
                 <?php
-                    $count++;
+                        $count++;
+                    }
+                } else {
                 }
-                    
-                }else{
-                }
-            ?>
+                ?>
 
             </tbody>
 
@@ -92,43 +96,56 @@
             <tbody>
 
                 <?php
-                $result = mysqli_query($con,"SELECT * FROM `lesson`");
-                $count=1;
+                $result = mysqli_query($con, "SELECT * FROM `lesson`");
+                $count = 1;
                 $rowCount = mysqli_num_rows($result);
-                if($rowCount > 0){
-                    while($row = mysqli_fetch_assoc($result)){
-                            $id=$row['lesson_id'];
-                                            ?>
-                <tr>
-                    <td style="width: 300px;"><?php echo $row['lesson_name']; ?></td>
-                    <td style="width: 300px;"><?php echo $row['lesson_desc']; ?></td>
-                    <td style="width: 300px;"><?php //echo $row['lesson_desc'];
+                if ($rowCount > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['lesson_id'];
+                ?>
+                        <tr>
+                            <td style="width: 300px;"><?php echo $row['lesson_name']; ?></td>
+                            <td style="width: 300px;"><?php echo $row['lesson_desc']; ?></td>
+                            <td style="width: 300px;"><?php //echo $row['lesson_desc'];
 
-                            $results = mysqli_query($con,"SELECT * FROM `lesson_view` WHERE `user_id` = '".$_SESSION['id']."' AND `lesson_id`= '".$id."'");
-                            $rows = mysqli_num_rows($results) ;
-                            if($rows > 0){
-                                echo '<span class="badge badge-success badge-pill">Done: View</span>';
-                            }else{
-                                echo '<span class="badge badge-danger badge-pill">Todo: View</span>';
-                            }
-                       
-                    ?></td>
-                    <td style="width: 300px;">
+                                                        $results = mysqli_query($con, "SELECT * FROM `lesson_view` WHERE `user_id` = '" . $_SESSION['id'] . "' AND `lesson_id`= '" . $id . "'");
+                                                        $rows = mysqli_num_rows($results);
+                                                        if ($rows > 0) {
+                                                            echo '<span class="badge badge-success badge-pill">Done: View</span>';
+                                                        } else {
+                                                            echo '<span class="badge badge-danger badge-pill">Todo: View</span>';
+                                                        }
 
-                        <a href="index.php?page=module-viewer&l=<?php echo $count; ?>">
-                            <button class=" btn btn-md btn-primary">
-                                <span class="icon-copy ti-eye"></span>
-                                Read</button>
-                        </a>
-                    </td>
-                </tr>
+                                                        ?></td>
+                            <td style="width: 300px;">
+                                <?php
+                                if ($count >= 2) {
+                                ?>
+                                    <a onclick="checkpoint()">
+                                        <button class=" btn btn-md btn-primary">
+                                            <span class="icon-copy fa fa-lock"></span>
+                                              Read</button>
+                                    </a>
+                                <?php
+                                } else {
+                                ?>
+                                    <a href="index.php?page=module-viewer&l=<?php echo $count; ?>">
+                                        <button class=" btn btn-md btn-primary">
+                                            <span class="icon-copy ti-eye"></span>
+                                            Read</button>
+                                    </a>
+                                <?php
+                                }
+                                ?>
+
+                            </td>
+                        </tr>
                 <?php
-                    $count++;
+                        $count++;
+                    }
+                } else {
                 }
-                    
-                }else{
-                }
-            ?>
+                ?>
 
             </tbody>
 
@@ -158,22 +175,30 @@
 
 
 <script>
-function delete_module(data_id) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this Data!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!',
-        closeOnConfirm: false,
-        closeOnCancel: false
-    }).then((result) => {
+    function delete_module(data_id) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this Data!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            closeOnConfirm: false,
+            closeOnCancel: false
+        }).then((result) => {
 
-        if (result.isConfirmed) {
-            window.location = ("action/admin/delete-module.php?id=" + data_id);
-        }
-    })
-}
+            if (result.isConfirmed) {
+                window.location = ("action/admin/delete-module.php?id=" + data_id);
+            }
+        })
+    }
 </script>
+<script>
+    function checkpoint() {
+        swal({
+            title: "Not Available",
+            icon: 'warning',
+        });
+    }
+    </script>
