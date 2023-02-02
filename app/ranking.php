71f16a93e1147883d36db78eebe6d3e6ddcfce14
parengjames <108077808+parengjames@users.noreply.github.com>
2023-02-02 17:57:32 +0800

    ranking page - admin side

--- app/ranking.php
+++ app/ranking.php
@@ -32,35 +32,79 @@
                     <th class="datatable-nosort">Hints used</th>
                     <th class="datatable-nosort">Score</th>
                     <th class="datatable-nosort">Remarks</th>
+                    <!-- <th class="datatable-nosort">Rank</th> -->
                 </tr>
             </thead>
             <tbody>
 
                 <?php
-                $result = mysqli_query($con,"SELECT * FROM `module`");
-                $count=1;
+                $result = mysqli_query($con, "SELECT users.firstname,users.lastname, exam.exam_title,exam_results.exam_attempt,exam_results.total_hintUsed, exam_results.Student_Score,exam_results.is_passed FROM exam_results JOIN users ON exam_results.student_id = users.user_id JOIN exam ON exam_results.exam_id = exam.exam_id WHERE exam_results.exam_attempt = 1 and users.role_id = 3 ORDER BY exam_results.Student_Score DESC;");
+                $count = 1;
                 $rowCount = mysqli_num_rows($result);
-                if($rowCount > 0){
-                    while($row = mysqli_fetch_assoc($result)){
-                            $id=$row['module_id'];
-                                            ?>
-                <tr>
-                    <td><?php// echo $row['module_name']; ?></td>
-                    <td><?php //echo $row['module_desc']; ?></td>
-                    <td><?php// echo $row['module_name']; ?></td>
-                    <td><?php //echo $row['module_desc']; ?></td>
-                    <td><?php// echo $row['module_name']; ?></td>
-                    <td><?php //echo $row['module_desc']; ?></td>
+                if ($rowCount > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                        <tr>
+                            <td><?php echo $row['firstname'] . "  " . $row['lastname'] ?></td>
+                            <td><?php echo $row['exam_title']; ?></td>
+                            <td><?php echo $row['exam_attempt']; ?></td>
+                            <td><?php echo $row['total_hintUsed']; ?></td>
+                            <td><?php echo $row['Student_Score']; ?></td>
+                            <td>
+                                <?php
+                                if ($row['Student_Score'] > 105 && $row['Student_Score'] <= 110) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #00FF00;border: none; border-radius: 25px;width: 100px;color: white;">
+                                        <span>Outstanding</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] > 100 && $row['Student_Score'] <= 105) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #00BFFF;border: none; border-radius: 25px;width: 78px;color: white;">
+                                        <span>Excellent</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] > 95 && $row['Student_Score'] <= 100) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #FFD700;border: none; border-radius: 25px;width: 78px;color: white;">
+                                        <span>Very good</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] > 90 && $row['Student_Score'] <= 95) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #FFA500;border: none; border-radius: 25px;width: 60px;color: white;">
+                                        <span>Good</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] > 85 && $row['Student_Score'] <= 90) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #9400D3;border: none; border-radius: 25px;width: 70px;color: white;">
+                                        <span>Average</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] >= 80 && $row['Student_Score'] <= 85) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #A52A2A;border: none; border-radius: 25px;width: 60px;color: white;">
+                                        <span>Poor</span>
+                                    </div>
+                                <?php
+                                } else if ($row['Student_Score'] < 80) {
+                                ?>
+                                    <div class="card text-center" style="padding: 2px;background-color: #DC143C;border: none; border-radius: 25px;width: 60px;color: white;">
+                                        <span>Failed</span>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                            </td>
 
-
-                </tr>
+                        </tr>
                 <?php
-                    $count++;
+                        $count++;
+                    }
+                } else {
                 }
-                    
-                }else{
-                }
-            ?>
+                ?>
 
             </tbody>
 
@@ -69,112 +113,23 @@
 </div>
 <!-- Simple Datatable End -->
 
-<!-- Add Modal -->
-<div class="modal fade" id="addModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form class="form-horizontal" action="action/admin/add-module.php" method="POST">
-                <div class="modal-header">
-                    <h4 class="modal-title">Add Course</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-
-                    <!-- form -->
-                    <div class="form-group row">
-                        <label for="course" class="col-sm-3 col-form-label">Course <span class="text-danger">
-                                *</span> </label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="module_name" name="module_name"
-                                placeholder="Module" required>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="module_desc" class="col-sm-3 col-form-label">Description <span class="text-danger">
-                                *</span> </label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="module_desc" name="module_desc"
-                                placeholder="Description" required>
-                        </div>
-                    </div>
-
-
-                </div>
-                <div class="modal-footer justify-content-right">
-                    <button type="button" class="btn btn-danger " data-dismiss="modal">Cancel</button>
-                    <button type="submit" name="add-module" class="btn btn-primary "> Add </button>
-                </div>
-
-            </form>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- Add Modal -->
-<div class="modal fade" id="addLesson">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form class="form-horizontal" action="action/admin/add-lesson.php" method="POST">
-                <div class="modal-header">
-                    <h4 class="modal-title">Add Lesson</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-
-                    <!-- form -->
-                    <div class="form-group row">
-                        <label for="lesson_name" class="col-sm-3 col-form-label">Lesson <span class="text-danger">
-                                *</span> </label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="lesson_name" name="lesson_name"
-                                placeholder="Lesson No." required>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="lesson_desc" class="col-sm-3 col-form-label">Description <span class="text-danger">
-                                *</span> </label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="lesson_desc" name="lesson_desc"
-                                placeholder="Description" required>
-                        </div>
-                    </div>
-
-
-                </div>
-                <div class="modal-footer justify-content-right">
-                    <button type="button" class="btn btn-danger " data-dismiss="modal">Cancel</button>
-                    <button type="submit" name="add-module" class="btn btn-primary "> Add </button>
-                </div>
-
-            </form>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-
 <script>
-function delete_module(data_id) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this Data!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!',
-        closeOnConfirm: false,
-        closeOnCancel: false
-    }).then((result) => {
+    function delete_module(data_id) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this Data!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            closeOnConfirm: false,
+            closeOnCancel: false
+        }).then((result) => {
 
-        if (result.isConfirmed) {
-            window.location = ("action/admin/delete-module.php?id=" + data_id);
-        }
-    })
-}
+            if (result.isConfirmed) {
+                window.location = ("action/admin/delete-module.php?id=" + data_id);
+            }
+        })
+    }
 </script>
